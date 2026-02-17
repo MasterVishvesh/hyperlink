ca9331afb84c690079d83fac54d9ae67f8de2212
mathurvishvesh <mathurvishvesh243@gmail.com>
2026-02-17 12:34:45 +0530

    update the database_url formate

--- database.py
+++ database.py
@@ -19,12 +19,17 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 # ENGINE CONFIG (SQLite Safe)
 # -----------------------------
 
+# engine = create_engine(
+#     DATABASE_URL,
+#     connect_args={
+#         "check_same_thread": False,
+#         "timeout": 30  # prevents locking errors
+#     },
+#     pool_pre_ping=True
+# )
+
 engine = create_engine(
     DATABASE_URL,
-    connect_args={
-        "check_same_thread": False,
-        "timeout": 30  # prevents locking errors
-    },
     pool_pre_ping=True
 )
 
